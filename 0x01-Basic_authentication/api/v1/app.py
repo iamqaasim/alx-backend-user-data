dd99ac97775e48e0840c2d0c14ad61f0dd77b0ba
iamqaasim <iamqaasim@gmail.com>
2023-05-02 14:13:16 -0700

    My first commit

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -56,13 +56,6 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Request unauthorized handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
