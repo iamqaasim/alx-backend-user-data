273e9a194cf45a9452fa63959d35ef4470849fbb
iamqaasim <iamqaasim@gmail.com>
2023-05-02 11:47:51 -0700

    My first commit

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,6 +4,13 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def authorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+        - raise a 401 error
+    """
+    abort(401, description="Unauthorized")
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
