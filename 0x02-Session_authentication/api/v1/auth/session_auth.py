8d8a46caf13db53ad2309bcb5227ff489dd87d48
iamqaasim <iamqaasim@gmail.com>
2023-05-04 19:51:31 +0200

    Task 1 - added session_auth folder and updated app.py for using the instance

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Definition of class SessionAuth
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Implement Session Authorization protocol methods
+    """
