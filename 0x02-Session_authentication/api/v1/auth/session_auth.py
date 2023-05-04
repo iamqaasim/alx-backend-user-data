51d1a7b7ce9bebff5479df2c3c984c65b7f08fd2
iamqaasim <iamqaasim@gmail.com>
2023-05-04 20:06:48 +0200

    Task 3 - created a user_id_for_session_id function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,4 +26,14 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[str(id)] = user_id
         return str(id)
     
-    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a user ID based on a session ID
+        Args:
+            session_id (str): session ID
+        Return:
+            user id or None if session_id is None or not a string
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
