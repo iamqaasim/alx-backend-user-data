bb35c64a803b6b8a9e2a82ad3264c313f1b0fd2c
iamqaasim <iamqaasim@gmail.com>
2023-05-04 19:59:21 +0200

    Task 2 - created a create_session function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,25 @@
 Definition of class SessionAuth
 """
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Implement Session Authorization protocol methods
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user with id user_id
+        Args:
+            user_id (str): user's user id
+        Return:
+            None is user_id is None or not a string
+            Session ID in string format
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
