3d2cb065c925ab08fdfe72378354778edcde4bf2
iamqaasim <iamqaasim@gmail.com>
2023-05-10 15:00:40 +0200

    Task 3 - created find_user_by funtion

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -46,3 +48,24 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user in the database based on the provided filter criteria
+
+        Args:
+            **kwargs: Arbitrary keyword arguments representing filter criteria
+
+        Returns:
+            User: The first User object matching the filter criteria
+        """
+        session = self._session  # Get the database session
+        try:
+            user = session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound("No user found based on the filter criteria")
+            return user
+        except InvalidRequestError as e:
+            session.rollback()
+            raise e
+    
