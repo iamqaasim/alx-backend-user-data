03f7817a1c59490e93abaf5dcc2499a199178890
iamqaasim <iamqaasim@gmail.com>
2023-05-10 21:23:05 +0200

    Task 4 - created hash_password funtion

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Definition of _hash_password function
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes the password with bcrypt and returns the salted hash"""
+    password = password.encode('utf-8')  # Convert password to bytes
+    salt = bcrypt.gensalt()  # Generate a salt
+    hashed_password = bcrypt.hashpw(password, salt)  # Hash the password with the salt
+    return hashed_password
