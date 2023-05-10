96904aa60594ceca77e4c914749fa0f34905ab18
iamqaasim <iamqaasim@gmail.com>
2023-05-10 20:03:27 +0200

    Task 2 - created add_user funtion

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,8 +44,8 @@ class DB:
         """
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
-        self._session.add(user)
-        self._session.commit()
+        session.add(user)
+        session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
