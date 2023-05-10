2be6f3ab3d98a461018a073c546c63e0f13fd864
iamqaasim <iamqaasim@gmail.com>
2023-05-10 21:53:11 +0200

    Task 5 - created register_user funtion

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,12 +7,14 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
+
 def _hash_password(password: str) -> bytes:
     """Hashes the password with bcrypt and returns the salted hash"""
     password = password.encode('utf-8')  # Convert password to bytes
     salt = bcrypt.gensalt()  # Generate a salt
     hashed = bcrypt.hashpw(password, salt)  # Hash the password with the salt
     return hashed
+
 
 class Auth:
     """Auth class to interact with the authentication database.
