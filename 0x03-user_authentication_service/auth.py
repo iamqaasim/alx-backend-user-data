7d9c20c91f4465d4fe7b4a0b785856033063b1a5
iamqaasim <iamqaasim@gmail.com>
2023-05-10 21:37:46 +0200

    Task 5 - created register_user funtion

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,9 @@
 Definition of _hash_password function
 """
 import bcrypt
-
+from db import DB
+from db_utils import hash_password
+from user import User
 
 def _hash_password(password: str) -> bytes:
     """Hashes the password with bcrypt and returns the salted hash"""
@@ -11,3 +13,16 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()  # Generate a salt
     hashed = bcrypt.hashpw(password, salt)  # Hash the password with the salt
     return hashed
+
+
+def register_user(self, email: str, password: str) -> User:
+    try:
+        existing_user = self._db.find_user_by(email=email)
+        if existing_user:
+            raise ValueError(f"User {email} already exists.")
+
+        hashed_password = hash_password(password)
+        user = self._db.add_user(email=email, hashed_password=hashed_password)
+        return user
+    except Exception as e:
+        raise e
