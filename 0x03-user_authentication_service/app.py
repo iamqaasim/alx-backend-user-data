562f5169a43e1dbcf4bd974747a965da5b163bb6
iamqaasim <iamqaasim@gmail.com>
2023-05-11 17:14:52 +0200

    Task 11 - created a login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,8 @@ Flask app
 from flask import (
     Flask,
     jsonify,
-    request
+    request,
+    abort
 )
 
 from auth import Auth
@@ -36,6 +37,24 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
     return jsonify({"email": f"{email}", "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """
+    login user
+    Returns:
+        str: JSON str
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
