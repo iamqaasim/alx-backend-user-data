2428a9b770e11f7333cd40a46639390ab3f37c3f
iamqaasim <iamqaasim@gmail.com>
2023-05-11 18:13:27 +0200

    Task 15 - created profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,5 +72,20 @@ def logout():
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """
+    Return a user's email based on session_id in the received cookies
+    Returns:
+        str: JSON str
+    """
+    session_id = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        email = user.email
+        return jsonify({"email": f"{email}"}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
